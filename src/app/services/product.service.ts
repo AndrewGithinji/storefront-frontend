63e7c9d52876b7b172f1edd15b23991efb91412d
AndrewGithinji <andrewgithinji@hotmail.com>
2023-03-13 18:26:36 +0300

    Product services

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/Product';
+
+@Injectable()
+export class ProductService {
+
+  baseUrl = '/api/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  getProduct(id: number): Observable<Product | null> {
+    return this.http.get<Product | null>(`${this.baseUrl}/${id}`);
+  }
+
+  searchProducts(term: string): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}?name=${term}`);
+  }
+}
