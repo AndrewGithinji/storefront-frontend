fd4e13541503150f4392393eb4faae86872ea107
AndrewGithinji <andrewgithinji@hotmail.com>
2023-03-13 21:52:35 +0300

    App Module Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -46,6 +46,9 @@ FormsModule,
 HttpClientModule,
 RouterModule,
 NgxSkeletonLoaderModule,
+],
+schemas: [
+  CUSTOM_ELEMENTS_SCHEMA
 ],
 providers: [],
 bootstrap: [AppComponent]
