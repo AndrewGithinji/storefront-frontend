8ae0980301d778bc30f413417e0fc096b2f7912a
AndrewGithinji <andrewgithinji@hotmail.com>
2023-03-13 18:34:53 +0300

    app components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
+import { CartProductComponent } from './cart-product/cart-product.component';
+import { ButtonComponent } from './button/button.component';
+import { AmountSelectorComponent } from './amount-selector/amount-selector.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ConfirmationComponent,
+    CheckoutFormComponent,
+    CartComponent,
+    CartProductComponent,
+    ButtonComponent,
+    AmountSelectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
